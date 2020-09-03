9b5f6baddc3f4510065c5179cd91692b56352eeb
Jwmffreitas <jwmf.freitas@gmail.com>
2020-09-03 17:57:24 -0300

    login-script

---
+++ NewDesignMobile-Vendeur-eCommerce/JS/script-login.js
@@ -0,0 +1,9 @@
+
+function ManterInput() {
+    if(document.getElementById('email').)
+    var email = document.getElementById('email').value
+
+    if(email.value != "") {
+        document.getElementById('email').style.width = "100%"
+    }
+}
