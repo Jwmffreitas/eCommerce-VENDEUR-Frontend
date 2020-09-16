821b73daf09b2a27e2994ff6636f8aeed614409b
Jwmffreitas <jwmf.freitas@gmail.com>
2020-09-16 09:38:33 -0300

    enviando dados pro console

--- NewDesignMobile-Vendeur-eCommerce/JS/script-signup.js
+++ NewDesignMobile-Vendeur-eCommerce/JS/script-signup.js
@@ -1,8 +1,19 @@
+var confirma = 'Not!'
+
+
 function verificarEmail () {
     if(document.getElementById('email').value == "") {
         document.getElementById('email').style.width = "30%"
     }else {
         document.getElementById('email').style.width = "100%"
+    }
+}
+
+function verificarNome () {
+    if(document.getElementById('nome').value == "") {
+        document.getElementById('nome').style.width = "30%"
+    }else {
+        document.getElementById('nome').style.width = "100%"
     }
 }
 
@@ -14,25 +25,20 @@ function verificarSenha () {
     }
 }
 
-function verificarEndereco () {
-    if(document.getElementById('ender').value == "") {
-        document.getElementById('ender').style.width = "160px"
+function confirmarSenha () {
+    if(document.getElementById('csenha').value == "") {
+        document.getElementById('csenha').style.width = "30%"
     }else {
-        document.getElementById('ender').style.width = "100%"
-    }
-}
-
-function verificarComp () {
-    if(document.getElementById('comp').value == "") {
-        document.getElementById('comp').style.width = "160px"
-    }else {
-        document.getElementById('comp').style.width = "160px"
+        document.getElementById('csenha').style.width = "100%"
+        if(document.getElementById('senha').value == document.getElementById('csenha').value) {
+            confirma = `Ok!`
+        }
     }
 }
 
 function verificarWhats () {
     if(document.getElementById('number').value == "") {
-        document.getElementById('number').style.width = "30%"
+        document.getElementById('number').style.width = "52%"
     }else {
         document.getElementById('number').style.width = "100%"
         var numero = document.getElementById('number').value
@@ -48,16 +54,11 @@ function verificarWhats () {
     }
 }
 
-/*function aumentarCEP() {
-    document.getElementById('cep').style.setProperty("width", "40%", "!important")
-    verificarCEP()
-}*/
-
 function verificarCEP () {
     if(document.getElementById('cep').value == "") {
         document.getElementById('cep').style.width = "30%"
     }else {
-        document.getElementById('cep').style.width = "40%"
+        document.getElementById('cep').style.width = "45%"
         var cep = document.getElementById('cep').value
         var resultado = cep.indexOf("-")
         console.log(resultado)
@@ -86,4 +87,39 @@ function verificarCEP () {
 
 function reativarCEP () {
     document.getElementById('cep').readOnly = false
+}
+
+function verificarEndereco () {
+    if(document.getElementById('ender').value == "") {
+        document.getElementById('ender').style.width = "160px"
+    }else {
+        document.getElementById('ender').style.width = "100%"
+    }
+}
+
+function verificarComp () {
+    if(document.getElementById('comp').value == "") {
+        document.getElementById('comp').style.width = "160px"
+    }else {
+        document.getElementById('comp').style.width = "160px"
+    }
+}
+
+/*function aumentarCEP() {
+    document.getElementById('cep').style.setProperty("width", "40%", "!important")
+    verificarCEP()
+}*/
+
+function enviarDados() {
+    console.log(`Email: ${document.getElementById('email').value}`)
+    console.log(`Nome: ${document.getElementById('nome').value}`)
+    console.log(`Senha: ${document.getElementById('senha').value}`)
+    console.log(`Confirmar Senha: ${confirma}`)
+    console.log(`WhatsApp: ${document.getElementById('number').value}`)
+    console.log(`CEP: ${document.getElementById('cep').value}`)
+    console.log(`Endereco: ${document.getElementById('ender').value}`)
+    console.log(`Complemento: ${document.getElementById('comp').value}`)
+    console.log(`UF: ${document.getElementById('UF').value}`)
+    console.log(`Cidade: ${document.getElementById('city').value}`)
+    console.log(`Bairro: ${document.getElementById('neigh').value}`)
 }
