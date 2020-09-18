fa85112cc7b7a3df3d0dbe05ff24213a6743f013
Jwmffreitas <jwmf.freitas@gmail.com>
2020-09-18 16:53:59 -0300

    chat funcionando

---
+++ NewDesignMobile-Vendeur-eCommerce/JS/script-chat.js
@@ -0,0 +1,19 @@
+
+function enviarMensagem() {
+    mensagem = document.getElementById('mensagem').value
+    if(mensagem != "") {
+        document.getElementById("chat").innerHTML += `<div class="mensagem">
+        <div class="balaoU">
+            <h5 style="margin: 0; float: left; width: 94%;">
+                ${mensagem}
+            </h5>
+            <div style="margin-top: 5px;">
+                <i class="fas fa-paper-plane" style="float: right; font-size: 15px;"></i>
+            </div>
+        </div>
+    </div>`
+    document.getElementById('mensagem').value = ""
+    }else {
+
+    }
+}
