cbf79eb993ed3be6b79c62e76c0bf09f704d5a59
Jwmffreitas <jwmf.freitas@gmail.com>
2020-09-25 12:42:53 -0300

    script brinco

--- NewDesignDesktop-Vendeur-eCommerce/JS/script.js
+++ NewDesignDesktop-Vendeur-eCommerce/JS/script.js
@@ -1,19 +1,20 @@
 alianca = 0
 anel = 0
+brinco = 0
 
 function mostrarAliancas() {
     if(alianca == 0) {
         $('.vAB').slideDown(500, function() {
             //$('.vB').css('display', 'flex')
-            $('.open').append('<i>')
-            $('.open i:first').attr('class', 'fas fa-times')
+            $('.openAL').append('<i>')
+            $('.openAL i:first').attr('class', 'fas fa-times')
         })
         alianca = 1
     }else {
         $('.vAB').slideUp(600)
         setTimeout(() => {
-            $('.open').append('<i>')
-            $('.open i:first').attr('class', 'fas fa-angle-down')
+            $('.openAL').append('<i>')
+            $('.openAL i:first').attr('class', 'fas fa-angle-down')
         }, 400)
         alianca = 0
     }
@@ -22,15 +23,31 @@ function mostrarAliancas() {
 function mostrarAneis() {
     if(anel == 0) {
         $('.vAC').slideDown(500, function() {
-            $('.openB').append('<i>')
-            $('.openB i:first').attr('class', 'fas fa-times')
+            $('.openAN').append('<i>')
+            $('.openAN i:first').attr('class', 'fas fa-times')
         })
         anel = 1
     }else {
         $('.vAC').slideUp(600, function() {
+            $('.openAN').append('<i>')
+            $('.openAN i:first').attr('class', 'fas fa-angle-down')
+        })
+        anel = 0
+    }
+}
+
+function mostrarBrincos () {
+    if(brinco == 0) {
+        $('.vAD').slideDown(500, function() {
+            $('.openB').append('<i>')
+            $('.openB i:first').attr('class' , 'fas fa-times')
+        })
+        brinco = 1
+    }else {
+        $('.vAD').slideUp(600, function() {
             $('.openB').append('<i>')
             $('.openB i:first').attr('class', 'fas fa-angle-down')
         })
-        anel = 0
+        brinco = 0
     }
 }
