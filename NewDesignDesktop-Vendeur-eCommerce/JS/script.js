4aeca96bdc80a19fac913f2921e64f1b077f6bc1
Jwmffreitas <jwmf.freitas@gmail.com>
2020-09-25 10:48:41 -0300

    resto da categoria aneis

--- NewDesignDesktop-Vendeur-eCommerce/JS/script.js
+++ NewDesignDesktop-Vendeur-eCommerce/JS/script.js
@@ -1,19 +1,36 @@
-x = 0
+alianca = 0
+anel = 0
 
 function mostrarAliancas() {
-    if(x == 0) {
+    if(alianca == 0) {
         $('.vAB').slideDown(500, function() {
             //$('.vB').css('display', 'flex')
             $('.open').append('<i>')
             $('.open i:first').attr('class', 'fas fa-times')
         })
-        x = 1
+        alianca = 1
     }else {
         $('.vAB').slideUp(600)
         setTimeout(() => {
             $('.open').append('<i>')
             $('.open i:first').attr('class', 'fas fa-angle-down')
         }, 400)
-        x = 0
+        alianca = 0
+    }
+}
+
+function mostrarAneis() {
+    if(anel == 0) {
+        $('.vAC').slideDown(500, function() {
+            $('.openB').append('<i>')
+            $('.openB i:first').attr('class', 'fas fa-times')
+        })
+        anel = 1
+    }else {
+        $('.vAC').slideUp(600, function() {
+            $('.openB').append('<i>')
+            $('.openB i:first').attr('class', 'fas fa-angle-down')
+        })
+        anel = 0
     }
 }
