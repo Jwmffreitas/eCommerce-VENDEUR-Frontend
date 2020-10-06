5cbb5f36234d451c1c663a3bf96b684d9f0b79ad
Jwmffreitas <jwmf.freitas@gmail.com>
2020-10-06 10:19:22 -0300

    design pagamento finalizado

---
+++ NewDesignDesktop-Vendeur-eCommerce/JS/script-carrinho.js
@@ -0,0 +1,10 @@
+function verificarNome () {
+    if(document.getElementById('nome').value == "") {
+        document.getElementById('nome').style.width = "30%"
+    }else {
+        document.getElementById('nome').style.width = "100%"
+    }
+}
+
+function ProxInput() {
+}
